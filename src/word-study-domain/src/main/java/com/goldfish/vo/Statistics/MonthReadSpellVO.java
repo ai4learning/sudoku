b0551bd5f39bc1f0f83bc492ae1366786a9af711
zhangjingtao <alex_zjt@163.com>
2018-06-20 20:57:36 +0800

    统计模块init

---
+++ src/word-study-domain/src/main/java/com/goldfish/vo/Statistics/MonthReadSpellVO.java
@@ -0,0 +1,42 @@
+package com.goldfish.vo.Statistics;
+
+/**
+ * Created by Administrator on 2018/6/20 0020.
+ */
+
+/**
+ * {
+ "date": "2018-5",
+ "totalReadTimes": 129,
+ "totalSpellTimes": 356
+ }
+ */
+public class MonthReadSpellVO {
+    private String date;
+    private int totalReadTimes;
+    private int totalSpellTimes;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getTotalReadTimes() {
+        return totalReadTimes;
+    }
+
+    public void setTotalReadTimes(int totalReadTimes) {
+        this.totalReadTimes = totalReadTimes;
+    }
+
+    public int getTotalSpellTimes() {
+        return totalSpellTimes;
+    }
+
+    public void setTotalSpellTimes(int totalSpellTimes) {
+        this.totalSpellTimes = totalSpellTimes;
+    }
+}
---
+++ src/word-study-web/src/main/java/com/goldfish/api/AjaxStatisticsController.java
@@ -0,0 +1,150 @@
+package com.goldfish.api;
+
+import com.goldfish.vo.Statistics.MonthVocStudyResultVO;
+import com.goldfish.vo.Statistics.TestResultVO;
+import com.goldfish.vo.Statistics.VocStudyResultVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Administrator on 2018/6/20 0020.
+ * 学习报告
+ * /api/Ajax/AjaxGetVocStudyResult
+ 最近一周所学单词
+ /api/Ajax/AjaxGetMonthVocStudyResult
+ 本月学习时间统计
+ /api/Ajax/AjaxGettestResult
+ 获取前15条测试成绩
+ /api/Ajax/AjaxGetAlltestResult
+ 获取所有测试成绩（最多300条）
+ */
+@Controller
+@RequestMapping("/api/Ajax")
+public class AjaxStatisticsController {
+    /**
+     * /api/Ajax/AjaxGetVocStudyResult
+     最近一周所学单词
+
+     入参
+     {}
+     出参
+     {
+     "data": [{
+     "date": "2018-05-23",
+     "voccount": 0
+     }, {
+     "date": "2018-05-24",
+     "voccount": 0
+     }],
+     "success": true,
+     "msg": "完成加载"
+     }
+     */
+    @RequestMapping(value = "AjaxGetVocStudyResult", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody
+    VocStudyResultVO doAjaxGetVocStudyResult(ModelMap context) {
+        return null;
+    }
+
+
+    /**
+     * /api/Ajax/AjaxGetMonthVocStudyResult
+     本月学习时间统计
+
+     入参
+     {}
+     出参
+     {
+     "data": [{
+     "date": "2018-5",
+     "totalReadTimes": 129,
+     "totalSpellTimes": 356
+     }],
+     "success": true,
+     "msg": "完成加载"
+     }
+     */
+    @RequestMapping(value = "AjaxGetMonthVocStudyResult", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody
+    MonthVocStudyResultVO doAjaxGetMonthVocStudyResult(ModelMap context) {
+        return null;
+    }
+
+
+    /**
+     * /api/Ajax/AjaxGettestResult
+     获取前15条测试成绩
+
+     入参
+     {}
+     出参
+     {
+     "data": [{
+     "Id": 0,
+     "resultScore": 0,
+     "createtime": "2018-05-28 23:12:47",
+     "testType": 5,
+     "unitNbr": "0",
+     "realDuration": 60,
+     "bookName": "自主测试"
+     }, {
+     "Id": 0,
+     "resultScore": 0,
+     "createtime": "2018-05-28 22:23:19",
+     "testType": 1,
+     "unitNbr": "1",
+     "realDuration": 240,
+     "bookName": "高考3500考纲优化四"
+     },
+     ...
+     ],
+     "success": true,
+     "msg": "完成加载"
+     }
+     */
+    @RequestMapping(value = "AjaxGettestResult", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody
+    TestResultVO doAjaxGettestResult(ModelMap context) {
+        return null;
+    }
+
+    /**
+     * /api/Ajax/AjaxGetAlltestResult
+     获取所有测试成绩（最多300条）
+
+     入参
+     {}
+     出参
+     {
+     "data": [{
+     "Id": 0,
+     "resultScore": 0,
+     "createtime": "2018-05-28 23:12:47",
+     "testType": 5,
+     "unitNbr": "0",
+     "realDuration": 60,
+     "bookName": "自主测试"
+     }, {
+     "Id": 0,
+     "resultScore": 0,
+     "createtime": "2018-05-28 22:23:19",
+     "testType": 1,
+     "unitNbr": "1",
+     "realDuration": 240,
+     "bookName": "高考3500考纲优化四"
+     },
+     ...
+     ],
+     "success": true,
+     "msg": "完成加载"
+     }
+     */
+    @RequestMapping(value = "AjaxGetAlltestResult", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody
+    TestResultVO doAjaxGetAlltestResult(ModelMap context) {
+        return null;
+    }
+}
