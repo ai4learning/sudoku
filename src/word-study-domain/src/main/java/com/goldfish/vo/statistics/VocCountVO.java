822d3ed6e205ebb828f9b323ddf1c5157d9cbeb9
wangyong <474960160@qq.com>
2018-07-08 15:54:49 +0800

    fix Upper bug

---
+++ src/word-study-domain/src/main/java/com/goldfish/vo/statistics/VocCountVO.java
@@ -0,0 +1,32 @@
+package com.goldfish.vo.statistics;
+
+/**
+ * Created by Administrator on 2018/6/20 0020.
+ */
+
+/**
+ * {
+ "date": "2018-05-23",
+ "voccount": 0
+ }
+ */
+public class VocCountVO {
+    private String date;
+    private int voccount;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getVoccount() {
+        return voccount;
+    }
+
+    public void setVoccount(int voccount) {
+        this.voccount = voccount;
+    }
+}
