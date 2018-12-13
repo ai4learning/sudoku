4d7b7c3a2b39c811e468a59859c0199e120e823c
ai4learning <jwptt10@gmail.com>
2018-12-13 21:41:51 +0800

    finalize delopy files for deploying

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
+usage()
+{
+cat <<HELP
+./deploy.sh [development|production] [options]
+development : deploy test enviroment
+production  : deploy a production enviroment
+-f : forcely restart all process
+HELP
+exit 1
+}
 
-src_dir=/home/sudoku/sudoku_ai4learning/src
+nargs=$#
+force_flag=0
+build_mode=development
+
+if [ $nargs -lt 1 ] || [ $nargs -gt 3 ] ; then
+  usage
+fi
+
+if [ $nargs = 2 ] ; then
+ if [ $2 = -f ] ; then
+  force_flag=1
+ else
+  usage
+ fi
+fi
+
+case $1 in
+ "development")
+    echo "development..."
+    build_mode="development"
+    ;;
+ "production")
+    echo "production..."
+    build_mode="production"
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+src_dir=/home/sudoku/src
 target_dir=/opt
 
 cd $src_dir/
@@ -11,13 +51,17 @@ parser="Already up-to-date"
 len=${#parser}
 if [ "$parser" == "${ret:0:$len}" ]
 then
- echo "Already up-to-date,exting" 
- exit 0
+ if [ $force_flag = 0 ] ; then
+  echo "Already up-to-date,ready to exitng without restarting service"
+  exit 0
+ else
+  echo "Already up-to-date,restart service by force"
+ fi
 fi
 echo "Ready to fetch lastest code"
 
 echo "Rebuilding project.."
-mvn clean package -Dmaven.test.skip=true -Pproduction
+mvn clean package -Dmaven.test.skip=true -P$build_mode
 
 sh "$target_dir/word-study/bin/shutdown.sh"
 sh "$target_dir/word-study-admin/bin/shutdown.sh"
